10f5badfc9748c9b75a697b58e7c04eb630cf8ed
Caleb Kenney <65873072+Joelkenn1@users.noreply.github.com>
2025-09-16 10:52:51 -0400

    Add files via upload

---
+++ public/dog.js
@@ -0,0 +1,43 @@
+function dogResults() {
+    window.addEventListener("DOMContentLoaded", () => {
+        const dogResults = JSON.parse(localStorage.getItem("dogResults"));
+
+            for(let i = 0; i < 10; i++) {
+               
+                const dogNum = document.getElementById(String(i));
+                const dogName = dogResults[i].replace(/[^a-zA-Z ]/g, '').replace(/ /g, '-');
+                
+                const link = document.createElement("a");
+
+                link.href = "https://www.petmd.com/dog/breeds/" + dogName.toLowerCase().slice(1);;
+                link.textContent = dogResults[i];
+                document.getElementById(String(i)).appendChild(link);
+                // dogNum.innerText = dogResults[i];
+               // dogNum.innerHTML = '<a href="https://www.petmd.com/dog/breeds/${dogResults[i]}">${dogResults[i]}</a>';
+            }
+        });
+}
+dogResults()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
